c142c188b12f6ae119499fee353dfabab8b7a827
Adarsh <adarshtrip2306@gmail.com>
2024-09-25 21:10:26 +0530

    push

---
+++ DAY1/str.js
@@ -0,0 +1,25 @@
+// //string creation
+// //1.using single quote
+// str='hello adarsh';
+// console.log(str)
+
+// //2.using double quote
+
+// str1='Hello tripathi';
+// console.log(str1);
+
+// //3.using backticks or string interpolation
+// st='Adarsh tripathi'
+// str2=`hello I\'m ${st}`;
+// console.log(str2);
+
+
+//excercise
+str3= 'My name is: ';
+str4='Adarsh tripathi';
+console.log(str3+str4);
+bill= `hi ${str3} ${str4} and The total bill is $${(1+5+3)}`;
+console.log(bill);
+console.log( `thank you
+come again`)   
+
