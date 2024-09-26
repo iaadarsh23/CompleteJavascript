ef7d5d0d8f14d78dae89a8e6f0e777fc06bdcae9
Adarsh <adarshtrip2306@gmail.com>
2024-09-26 17:48:17 +0530

    push

---
+++ Day-2/var.js
@@ -0,0 +1,15 @@
+// //Variables
+// let var1=5;
+// let calculation = 2+2;
+// console.log(calculation);
+// let str= 'hello adarsh'
+// console.log(str);
+
+
+// //reassigning a variable
+// var1='tripathi';
+// console.log(var1);
+
+//Project
+
+let showcount=0;
