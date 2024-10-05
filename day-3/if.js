03b4d26884bf04211aab194276a26cd8b308e396
Adarsh <adarshtrip2306@gmail.com>
2024-10-05 20:39:31 +0530

    push

---
+++ day-3/if.js
@@ -0,0 +1,31 @@
+//excercises
+
+//p1
+// const hour= 6;
+// if(hour>=6 && hour<=12){
+//     console.log("Good morning");   
+// }else if(hour>=13 && hour<=17){
+//     console.log("Good afternoon");    
+// }else{
+//     console.log("Good night");   
+// }
+
+//p2
+// const hour= 6;
+// const name= 'Adarsh'
+// if(hour>=6 && hour<=12){
+//     console.log(`Good morning ${name}`);   
+// }else if(hour>=13 && hour<=17){
+//     console.log(`Good afternoon ${name}`);    
+// }else{
+//     console.log(`Good night ${name}`);   
+// }
+
+const age= 15;
+if(age<=6|| age>=65){
+    console.log("lele discount");
+    
+}else{
+    console.log('nhi dunga discount ja jo krna hai krle saade');
+    
+}
