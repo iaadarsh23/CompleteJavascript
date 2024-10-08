5fe81bc70ce8a3859229855f35a4f5e6a2d9dd63
Adarsh <adarshtrip2306@gmail.com>
2024-10-08 22:29:42 +0530

    push

--- day-4/practice.js
+++ day-4/practice.js
@@ -4,3 +4,17 @@ function volume(length, breadth , height){
     console.log( length*breadth*height)
 }
 volume(10,20,40);
+
+//excercises-
+
+//p1
+function greet(name = 'there!', greetings= 'Hello'){
+   console.log(`${greetings} Mr ${name}`);
+   
+}
+greet('Adarsh',"good morning");
+greet('Adarsh');
+greet(undefined,'good evening');
+
+
+//p2
