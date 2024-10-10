7a1d146dadcdbdf852055c3b6971acbf69d8c379
Adarsh <adarshtrip2306@gmail.com>
2024-10-10 20:07:17 +0530

    push

--- day-4/practice.js
+++ day-4/practice.js
@@ -19,25 +19,35 @@
 
 //p2
 
-function convertTemperature(degree,unit){
-    if(unit==='C'){
-        const convert= convertToFahrenheit(degree);
-    }
-    else{
-        const convert = convertToCelsius(degree);
-    }
-}
-convertTemperature(100,'C')
+// function convertTemperature(degree,unit){
+//     if(unit==='C'){
+//         const convert= convertToFahrenheit(degree);
+//     }
+//     else{
+//         const convert = convertToCelsius(degree);
+//     }
+// }
+// convertTemperature(100,'C')
 
-function convertToFahrenheit(celsius){
-    const conversionInFahrenheit = (celsius*9/5)+32;
-    console.log(conversionInFahrenheit);
+// function convertToFahrenheit(celsius){
+//     const conversionInFahrenheit = (celsius*9/5)+32;
+//     console.log(conversionInFahrenheit);
     
-}
+// }
 
-function convertToCelsius(Fahrenheit){
-    const conversionIncelsius = (Fahrenheit-32)*5/9;
-    console.log(conversionIncelsius);
+// function convertToCelsius(Fahrenheit){
+//     const conversionIncelsius = (Fahrenheit-32)*5/9;
+//     console.log(conversionIncelsius);
     
-}
+// }
+//p4
 
+function convertLength(length, from, to){
+    if(from==='miles'){
+        console.log(`${(length*1.6)} km`);        
+    } else{
+        console.log(`${(length*0.621371)} miles`);
+        
+    }
+} 
+convertLength(50,'miles', 'km');
