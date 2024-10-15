0e1d8352b68d401066100cfc1406bebedf16a373
Adarsh <adarshtrip2306@gmail.com>
2024-10-15 22:30:43 +0530

    push

---
+++ day-6/dom.js
@@ -0,0 +1,8 @@
+console.log(document.title);
+document.title= 'dom manipulation';
+console.log(document.title);
+
+
+console.log(document.body);
+// document.body.innerHTML= '<button> hello </button>';
+console.log(document.body.innerHTML);
