3510433759275c87d9d1786510bb0d5ce3384811
Adarsh <adarshtrip2306@gmail.com>
2024-10-16 23:17:20 +0530

    push

--- day-6/dom.js
+++ day-6/dom.js
@@ -7,3 +7,5 @@ console.log(document.body);
 // document.body.innerHTML= '<button> hello </button>';
 console.log(document.body.innerHTML);
 console.log(document.body.innerHTML);
+console.log(document.body.innerHTML);
+
