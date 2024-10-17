22a35fec1f1d57672d9dd7222e2c2ee459125dda
Adarsh <adarshtrip2306@gmail.com>
2024-10-17 20:02:48 +0530

    push

--- day-6/dom.js
+++ day-6/dom.js
@@ -2,20 +2,7 @@ function subscribe(){
     const buttonElement= document.querySelector('.bt');
     if(buttonElement.innerHTML==='Subscribe'){
         buttonElement.innerHTML='Subscribed';
-
-        displayConfetti();
-
     }else{
         buttonElement.innerHTML='Subscribe';
     }   
-}
-
-function displayConfetti() {
-    const confetti = document.getElementById('confetti');
-    confetti.classList.add('show'); // Add class to display confetti
-    
-    // Remove the confetti after 3 seconds (or however long you want the celebration)
-    setTimeout(() => {
-        confetti.classList.remove('show');
-    }, 3000);
 }
