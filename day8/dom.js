f85e4fc825363fe6e69611d3407778f1f52c5370
Adarsh <adarshtrip2306@gmail.com>
2024-10-19 20:06:48 +0530

    push

--- day8/dom.js
+++ day8/dom.js
@@ -1,6 +1,10 @@
-//getElementByID
-console.log(document.getElementById('js-p'));
+// //getElementByID
+// console.log(document.getElementById('js-p'));
 
- //getElementByclass
- // it is a collectiin(array like objects= "node-list")'
- console.log(typeof document.getElementsByClassName('js-h1'));
+//  //getElementByclass
+//  // it is a collectiin(array like objects= "node-list")'
+//  console.log(typeof document.getElementsByClassName('js-h1'));
+//  console.log(document.querySelectorAll('js-h1'));
+
+ const newButton= document.querySelector('.js-h1');
+ newButton.innerHTML= '<button>click me</button>'
