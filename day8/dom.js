2df3e43692e6002ec7669e79f83a09f3ba1eda96
Adarsh <adarshtrip2306@gmail.com>
2024-10-19 20:35:58 +0530

    push

--- day8/dom.js
+++ day8/dom.js
@@ -6,5 +6,21 @@
 //  console.log(typeof document.getElementsByClassName('js-h1'));
 //  console.log(document.querySelectorAll('js-h1'));
 
- const newButton= document.querySelector('.js-h1');
- newButton.innerHTML= '<button>click me</button>'
+//  const newButton= document.querySelector('.js-h1');
+//  newButton.innerHTML= '<button>click me</button>';
+
+//  //outerHtml
+
+//  const outerHtml= document.querySelector('p');
+//  const innerhtml=outerHtml.outerHTML= '<div> <p> this is use case of outerhtml</p></div>'
+// console.log(innerhtml.innerHTML= ' i have used innerhtml under the outerhtml');
+
+//adding new element
+
+const content= document.querySelector('.js-h1');
+
+console.log(content);
+
+const addNewElement= document.createElement('button');
+content.appendChild(addNewElement)
+
