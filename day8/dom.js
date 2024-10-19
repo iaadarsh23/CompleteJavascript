d122be6c98be3fd5cf6a87b103e3dc95bdac0332
Adarsh <adarshtrip2306@gmail.com>
2024-10-19 19:21:03 +0530

    push

--- day8/dom.js
+++ day8/dom.js
@@ -1,1 +1,6 @@
-    
+//getElementByID
+console.log(document.getElementById('js-p'));
+
+ //getElementByclass
+ // it is a collectiin(array like objects= "node-list")'
+ console.log(typeof document.getElementsByClassName('js-h1'));
