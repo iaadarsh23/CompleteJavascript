be57426ebeb4a94a3bd50ae049c0ae480c64ed4d
Adarsh <adarshtrip2306@gmail.com>
2024-10-23 23:03:06 +0530

    push

--- day-9/event.js
+++ day-9/event.js
@@ -1,23 +1,22 @@
 const select = document.querySelectorAll('.js-h2');
+//we r using for loop because the queryselectorall returns the nodelist thats why we have to traverse it
 for(let i= 0; i<select.length; i++){
-    select[i].addEventListener('click', colorChanged);
+    select[i].addEventListener('click', colorchanged);
 }
-let isRed = false;
+function colorchanged(event) {
+    event.target.style.color = 'red';
+}
 
-function colorChanged(event) {
-    const target = event.target;
+let isred= false;
+const deselect= document.body;
+deselect.addEventListener('click', colorback);
 
-    // Toggle text color between red and black
-    target.style.color = (target.style.color === 'red') ? 'black' : 'red';
-    
-    // Toggle background color
-    if (isRed) {
-        document.body.style.backgroundColor = 'white';
-    } else {
-        document.body.style.backgroundColor = 'black';
+function colorback(){
+    if(isred){
+        document.body.style.backgroundColor='white';
     }
-    
-    isRed = !isRed;
+    else{
+        document.body.style.backgroundColor='black';
+    }
+    isred=!isred;
 }
-
-
