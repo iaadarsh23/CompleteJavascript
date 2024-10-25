ee6c31d5a6010e8cad455849373a2385a8a5a87e
Adarsh <adarshtrip2306@gmail.com>
2024-10-25 23:21:41 +0530

    push

--- day-9/phrases.js
+++ day-9/phrases.js
@@ -1,2 +1,12 @@
-
+const select= document.querySelector('.js-p');
+select.addEventListener('click', scrolled,true)
+isclicked= false;
+ function scrolled(event){
+    if(isclicked){
+        select.innerHTML='because u r chutiya';
+    }
+    else{
+        select.innerHTML='why the fuck i am not working hard?';
+    }
+    isclicked=!isclicked 
  }
