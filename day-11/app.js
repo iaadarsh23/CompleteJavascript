7173983a92b919ad4ba06fe516c48c4e88c2e2b9
Adarsh <adarshtrip2306@gmail.com>
2024-10-31 18:39:50 +0530

    push

--- day-11/app.js
+++ day-11/app.js
@@ -1,7 +1,14 @@
 const newDiv= document.createElement('div');
+newDiv.addEventListener('click', press)
 
-for(let i=0;i<100;i++){
-    const newPara= document.createElement('p');
-    newPara.textContent= 'JAI MATA DI'+i    
-    newPara.addEventListener()
+function press(event){
+    for(let i=0;i<100;i++){
+        const newPara= document.createElement('p');
+        newPara.textContent= 'JAI MATA DI'+i    
+        newPara.addEventListener('click',function(event){
+            console.log('happy diwali')
+        })
+        newDiv.appendChild(newPara)
+    }
+    
 }
