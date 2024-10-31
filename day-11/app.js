1c15bf8bd33a0c36a33ac2f84d152edaa1a7beb4
Adarsh <adarshtrip2306@gmail.com>
2024-10-31 18:33:06 +0530

    happy diwali

---
+++ day-11/app.js
@@ -0,0 +1,7 @@
+const newDiv= document.createElement('div');
+
+for(let i=0;i<100;i++){
+    const newPara= document.createElement('p');
+    newPara.textContent= 'JAI MATA DI'+i    
+    newPara.addEventListener()
+}
