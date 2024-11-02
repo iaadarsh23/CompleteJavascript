ce54fa27b8fcecf2ca94b07cbffba822f46bc965
Adarsh <adarshtrip2306@gmail.com>
2024-11-02 17:46:32 +0530

    push

--- day-12/eventloop.js
+++ day-12/eventloop.js
@@ -1,6 +1,8 @@
-const setTime= setTimeout(hello,5000)
+console.log('this is adarsh tripathi')
+const setTime= setTimeout(hello,8000)
 
 function hello(){
     console.log('hello everyone')
 
 }
+console.log('i will be the best software engineer');
