199f68fee621f80e075aa784227da9f93b61a0d4
Adarsh <adarshtrip2306@gmail.com>
2024-11-02 15:10:28 +0530

    push

--- day-11/app.js
+++ day-11/app.js
@@ -1,11 +1,31 @@
-const newDiv= document.createElement('div');
+// // measuring the performance of code in js
+// let t1= performance.now()
+// const newDiv= document.createElement('div');
+// for(let i=0;i<100;i++){
+//     const newPara= document.createElement('p');
+//     newPara.textContent= 'JAI MATA DI'+i    
+//     newPara.addEventListener('click',function(event){
+//         console.log('happy diwali')
+//     })
+//     newDiv.appendChild(newPara)
+// }
+// document.body.appendChild(newDiv);
+// let t2= performance.now()
+// console.log('this took ' + (t2-t1) + "ms");
 
-for(let i=0;i<100;i++){
-    const newPara= document.createElement('p');
-    newPara.textContent= 'JAI MATA DI'+i    
-    newPara.addEventListener('click',function(event){
-        console.log('happy diwali')
-    })
-    newDiv.appendChild(newPara)
+
+const userInput= document.querySelector('js-int')
+
+
+
+
+
+
+function tables(event){
+    for(let i=1;i<=10;i++){
+        let table= n*i;
+        console.log(table)
+    }
 }
-document.body.appendChild(newDiv);
+    
+
