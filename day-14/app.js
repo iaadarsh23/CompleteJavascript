b26809c9e112e1d3d899fff9d1162184053e6037
Adarsh <adarshtrip2306@gmail.com>
2024-11-06 22:46:54 +0530

    pust

---
+++ day-14/app.js
@@ -0,0 +1,14 @@
+const x = ()=>{
+    let name= 'adarsh';
+    const y= ()=>{
+        name= 'sonu'
+        console.log(name)
+    }
+    //means we are returning the y function in another variable outsidè  the scope
+     return y;
+}
+const z= x();
+console.log(z)
+
+//here z becomes a fn because it holds the reference oy fn y
+z();
