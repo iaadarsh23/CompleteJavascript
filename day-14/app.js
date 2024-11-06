b87919a7d4da460dd306047c57fc54f63472eecb
Adarsh <adarshtrip2306@gmail.com>
2024-11-06 23:09:23 +0530

    push

--- day-14/app.js
+++ day-14/app.js
@@ -1,14 +1,32 @@
-const x = ()=>{
-    let name= 'adarsh';
-    const y= ()=>{
-        let name= 'sonu';
-        console.log(name)
-    }
-    //means we are returning the y function in another variable outsidè  the scope
-     return y;
-}
-const z= x();
-console.log(z)
+// const x = ()=>{
+//     let name= 'adarsh';
+//     const y= ()=>{
+//         let name= 'sonu';
+//         console.log(name)
+//     }
+//     //means we are returning the y function in another variable outsidè  the scope
+//      return y;
+// }
+// const z= x();
+// console.log(z)
 
 //here z becomes a fn because it holds the reference oy fn y
+// z();
+
+//case-2
+
+const a= ()=>{
+    let name= 'adarsh';
+    const b= ()=>{
+        let naam='sonu';
+        const c= ()=>{
+            console.log(name,naam);
+        }
+        return c;
+    }
+    return b;
+}
+
+const z= a();
+console.log(z)
 z();
