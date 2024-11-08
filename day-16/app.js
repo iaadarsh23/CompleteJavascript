dc475ed22c64350e869e79073704d704bc522bf7
Adarsh <adarshtrip2306@gmail.com>
2024-11-08 15:46:20 +0530

    push

--- day-16/app.js
+++ day-16/app.js
@@ -1,21 +1,36 @@
 //lets make a todo list
+ let dataArray= [];
 function toDo(){
     const userInput= document.getElementById('data');
     const addTaskbtn= document.getElementById('add');
     const delTaskbtn= document.getElementById('Delete');
     const result= document.getElementById('js-result');
-    let dataArray= [];
+    
     addTaskbtn.addEventListener('click', adding);
     delTaskbtn.addEventListener('click',deleted)
+ 
     function adding(){
         dataArray.push(userInput.value);
-        result.innerHTML=dataArray; 
-        dataArray.value=''    
+        renderList();
+        userInput.value='';
     }
     function deleted(){
         dataArray.pop(userInput.value);
-        result.innerHTML=dataArray;
-         
+        renderList();
     }
+    //showing each item one after another
+    function renderList(){
+        result.innerHTML='';
+        for(let i= 0;i<dataArray.length;i++){
+            //at every iteration we will create a html list and update the list;
+            const newList = document.createElement('li');
+            newList.textContent=dataArray[i];
+            result.appendChild(newList);
+
+        
+        }
+    }
+
+
 }
 toDo();
