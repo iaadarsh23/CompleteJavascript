d0c4867e923adf91e085437f782f75d63afe96b0
Adarsh <adarshtrip2306@gmail.com>
2024-11-08 10:09:49 +0530

    push

---
+++ day-16/practice.js
@@ -0,0 +1,14 @@
+//creating an array
+
+const myArray= [10,'adarsh','ayush'];
+function array(){
+    const newElement=document.createElement('button');
+    let result= document.getElementById('js-result');
+    newElement.innerText= 'click to see output of an array';
+    document.body.appendChild(newElement);
+    newElement.addEventListener('click', data());
+
+    function data(){
+        result.innerHTML= myArray;
+    }
+}
